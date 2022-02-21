34a0c556d366d01ae4132555271905e6a49fe405
Miguel Jimenez <miguel.jimenez>
2022-02-21 18:29:24 -0500

    Adding EF with SQLite

---
+++ Travel/src/core/Travel.Domain/Enums/Currency.cs
@@ -0,0 +1,12 @@
+﻿namespace Travel.Domain.Enums
+{
+    public enum Currency
+    {
+        PHP,
+        USD,
+        JPY,
+        EUR,
+        NOK,
+        COP
+    }
+}
--- Travel/src/presentation/Travel.WebApi/Startup.cs
+++ Travel/src/presentation/Travel.WebApi/Startup.cs
@@ -6,11 +6,13 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using Travel.Data.Context;
 
 namespace Travel.WebApi
 {
@@ -26,6 +28,7 @@ namespace Travel.WebApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<TravelDbContext>(options => options.UseSqlite("Data Source=TravelTourDatabase.sqlite3"));
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
